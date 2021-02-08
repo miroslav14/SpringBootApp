c9f2db12504ed0c4192f427c49d1ab4be14c698e
Miroslav <Miroslav@Miroslav>
2021-02-08 17:27:39 +0000

    First commit of whole project into the repository

---
+++ src/main/java/spring_boot/json_processor/mutli_layer_tech/SpringBootMutliLayerTechApplication.java
@@ -0,0 +1,13 @@
+package spring_boot.json_processor.mutli_layer_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMutliLayerTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMutliLayerTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_boot/json_processor/mutli_layer_tech/SpringBootMutliLayerTechApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.json_processor.mutli_layer_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMutliLayerTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
